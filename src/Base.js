d0c2b4163962ec317cc966338ccd420b0df13ebc
Samuel_Kelechi <kcfreshie@ymail.com>
2021-03-19 06:55:48 +0100

    new updates done

---
+++ src/Base.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+import "firebase/firestore";
+
+export const app = firebase.initializeApp({
+  apiKey: "AIzaSyBU3dGdIS3ezXYZIQBiwldMShTHxCJdB14",
+  authDomain: "lotsngo.firebaseapp.com",
+  projectId: "lotsngo",
+  storageBucket: "lotsngo.appspot.com",
+  messagingSenderId: "328018434853",
+  appId: "1:328018434853:web:9dbe42741cb10bee0b58cc",
+});
---
+++ src/Home/Home/HomeStart.js
@@ -0,0 +1,78 @@
+import { Button } from "antd";
+import React from "react";
+import img from "../videos/img.jpg";
+import hero from "../videos/LotsHomePhoto-removebg-preview.png";
+
+const HomeStart = () => {
+  return (
+    <center>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          width: "100%",
+          justifyContent: "center",
+          backgroundColor: "#F4F6FC",
+          marginTop: "80px",
+        }}
+      >
+        <div
+          style={{
+            // backgroundColor: "red",
+            width: "300px",
+            height: "240px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "24px",
+              textAlign: "Left",
+              paddingLeft: "15px",
+              marginBottom: "20px",
+              color: "rgb(168,142,83)",
+              fontWeight: "bold",
+              marginTop: "50px"
+            }}
+          >
+            LOTS Charity Foundation
+          </div>
+          <Button
+            style={{
+              fontSize: "20px",
+              fontWeight: "bold",
+              height: "50px",
+              textTransform: "uppercase",
+              width: "100%",
+              // marginLeft: "5px",
+              backgroundColor: "rgb(220,226,249)",
+              // color: "white",
+            }}
+          >
+            Raising 7 Star Performance
+          </Button>
+        </div>
+        <div
+          style={{
+            // backgroundColor: "blue",
+            width: "320px",
+            height: "250px",
+          }}
+        >
+          <img
+            src={hero}
+            alt="bckimage"
+            style={{
+              width: "100%",
+              height: "250px",
+              objectFit: "contain",
+            }}
+          />
+        </div>
+      </div>
+    </center>
+  );
+};
+
+export default HomeStart;
